d87f838ff9917e1bd263be768e0ddb12676b831c
Elias Lombart <elias.lombart@ugent.be>
2026-06-05 21:22:54 +0200

    project restructure added a new example

---
+++ examples/basic/01_general_usage.py
@@ -0,0 +1,46 @@
+from milii import *
+
+sigil(
+    simpleparser(end=" ", cast=int),
+    sigil="$"
+)
+
+sigil(
+    simpleparser(end=" "),
+    sigil=".",
+    executable=True
+)
+
+@sigil(sigil='"')
+def string_parser(code):
+    index = 1
+    buffer = ""
+
+    while index < len(code):
+        if code[index] == '"':
+            break
+
+        if code[index] == "\\":
+            index += 1
+
+        if index >= len(code):
+            break
+
+        buffer += code[index]
+
+        index += 1
+
+    return buffer, index
+
+@builtin
+def echo(stack):
+    print(stack.pop())
+
+@builtin
+def add(stack):
+    a, b = stack.pop_n(2)
+    return a + b
+
+run(
+    '$4 $5 .add .echo "Hello " "World!" .add .echo'
+)
